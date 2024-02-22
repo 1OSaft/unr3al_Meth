1c70833d49e19a4005af642cad49d9e68556fc7e
Tom <111147401+1OSaft@users.noreply.github.com>
2024-02-22 22:49:15 +0100

    Delete sql.sql

--- sql.sql
+++
@@ -1,6 +0,0 @@
-
-INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
-	('acetone', 'Acetone', 1, 0, 1),
-	('methlab', 'Portable Methlab', 1, 0, 1),
-	('meth', 'Meth', 0.5, 0, 1),
-	('lithium', 'Lithium Batteries', 2, 0, 1);
